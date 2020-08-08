25b8ec7e5ee95986f028db797b9cb98620c84804
Jean-Loup Johnston <jeanloupj94@gmail.com>
2020-08-07 23:23:36 -0400

    Fixed server path

--- static/scripts.js
+++ static/scripts.js
@@ -2,7 +2,7 @@ var board;
 
 var currentPlayer =""
 var playerTurn = "X"
-var socket = io.connect('http://ultimatetictactoes.com')
+var socket = io.connect('http://'+document.domain +":" +location.port)
 const cells = document.querySelectorAll(".cell");
 
 //start game
